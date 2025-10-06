dfe28e0b258ce387c21f5e4f32521859c670b5f6
gaaaabz <gaby8gomes@gmail.com>
2025-10-06 14:14:15 -0300

    Camada bussiness criada junto com uma interface generica para se encaixar com todas as classes, e realizar crud completo

---
+++ OrangeChat/OrangeChatModel/StatusModel.cs
@@ -0,0 +1,12 @@
+namespace OrangeChat.Domain.Models;
+public class StatusModel
+{
+    public string? Id { get; set; }
+    public required string NomeStatus { get; set; }
+    public string? Descricao { get; set; }
+
+    public StatusModel()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+}
